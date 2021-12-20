8d2566d5ab74f6d9889c56d0a86f7f1d7f213eb6
Jérôme Coupé <jercoupe@gmail.com>
2021-12-20 13:37:55 +0100

    team and blog collections + global and directory data files

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,34 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
-  // collections
-  eleventyConfig.addCollection("team", function (collection) {
-    return collection.getFilteredByGlob("./src/content/team/**/*.md");
+  // collection team
+  eleventyConfig.addCollection("team", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("./src/content/team/**/*.md")
+      .sort((a, b) => a.data.surname.localeCompare(b.data.surname));
   });
+
+  // collection blogposts
+  eleventyConfig.addCollection("blogposts", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("./src/content/blogposts/**/*.md")
+      .reverse();
+  });
+
+  // filter dateFull
+  eleventyConfig.addFilter("dateFull", function (jsDate, locale = "en") {
+    let dt = DateTime.fromJSDate(jsDate);
+    return dt.setLocale(locale).toLocaleString(DateTime.DATE_FULL);
+  });
+
+  // filter dateISO
+  eleventyConfig.addFilter("dateISO", function (jsDate) {
+    let dt = DateTime.fromJSDate(jsDate);
+    return dt.toISODate();
+  });
+
+  // copy files
+  eleventyConfig.addPassthroughCopy("./src/assets/img");
 
   // override default config
   return {
--- src/content/team/team.11tydata.js
+++ src/content/team/team.11tydata.js
@@ -1,3 +1,17 @@
+/**
+ * Eleventy directory data files
+ * https://www.11ty.dev/docs/data-template-dir/
+ *
+ * Comme ce fichier s'appelle
+ * nomDuDossierParent.11tydata.js
+ * Toutes les key / value pairs vont être traitée par Eleventy comme si elles étaient dans chacun des fichiers de ce dossier
+ *
+ * Dans ce cas-ci le dossier parent = team
+ * Donc Eleventy va traiter les keys/values dans team.11tydata.js comme si elles étaient dans chacun des fichiers Markdown (jerome.md, marie.md, etc.)
+ */
 module.exports = {
   layout: "layouts/team.njk",
+  // page.fileSlug == le nom de fichier (moins la date éventuelle)
+  // https://www.11ty.dev/docs/data-eleventy-supplied/#fileslug
+  permalink: "team/{{ page.fileSlug }}/index.html",
 };
